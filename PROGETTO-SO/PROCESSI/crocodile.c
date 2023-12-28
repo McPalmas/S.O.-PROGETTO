801775b3a2de516c9e2cab1c7a9c4c0ac7dd2473
Federico Giugni <f.giugni@studenti.unica.it>
2023-12-28 17:31:45 +0100

    Comportamento crocodile
    Definiti i comportamenti:
    - Timer tempo di permanenza rana sul coccodrillo
    - Switch a malvagio del coccodrillo
    - Immersione del coccodrillo (Solo la condizione)

--- PROGETTO-SO/PROCESSI/crocodile.c
+++ PROGETTO-SO/PROCESSI/crocodile.c
@@ -45,27 +45,41 @@ void crocodile_process(int id, int pipe[2], int pipe_crocodile_position[2], int 
     crocodile.direction = rand() % 2 ? RIGHT : LEFT;
 
     // tempo minimo di attesa del crocodile prima di diventare malvagio
-    int frog_on_crocodile_min_time = 80;
-    // tempo minimo di attesa del crocodile prima di sparire
+    int crocodile_switch_min_time= 80;
+    // tempo minimo di attesa del crocodile prima di immergersi
     int crocodile_vanish_min_time =  50;
     
     // tempo  effettivo di attesa del crocodile prima di diventare malvagio
-    int frog_on_crocodile_timer = frog_on_crocodile_min_time + rand() % 130;
-    // tempo effettivo di attesa del crocodile prima di sparire
-    int crocodile_switch_timer = crocodile_vanish_min_time + rand() % 100;
+    int crocodile_switch_timer = crocodile_switch_min_time + rand() % 130;
+    // tempo effettivo di attesa del crocodile prima di immergersi
+    int crocodile_vanish_timer = crocodile_vanish_min_time + rand() % 100;
 
 
 
     while(1){
-        
-        // se il crocodile tocca il bordo sinistro dell'area di gioco
-        if(crocodile.x <= 0){
-            // cambia lato
-            crocodile.x=  MAXX - CROCODILE_W;
+        // Se è presente il dato (Frog è su di lui)
+        if(read(pipe_frog_on_crocodile[0], &frog_data, sizeof(Position)) != -1){
+            // Se il crocodile è buono, diventa malvagio dopo il tempo definito da crocodile_switch_timer
+            if(crocodile.is_good == true){
+                crocodile_switch_timer--;
+                if(crocodile_switch_timer == 0){
+                    crocodile.is_good = false;
+                }
+            }
+            // Se il crocodile è malvagio, si immerge dopo il tempo definito da crocodile_vanish_timer
+            else{
+                crocodile_vanish_timer--;
+                if(crocodile_vanish_timer == 0){
+                    // si immerge
+                }
+            }
         }
-        else if(crocodile.x>= MAXX - CROCODILE_W){
-            // cambia lato
-            crocodile.x = 0;
+        
+
+
+        // se il crocodile tocca il bordo sinistro o destro dell'area di gioco, il suo processo viene terminato
+        if(crocodile.x < 0 || crocodile.x >= MAXX - CROCODILE_W){
+            exit(0);
         }
 
         // spostamento del crocodile
