76d1f37f5d279c58ce5d94e13b0d8a2052a0b2bd
Federico Giugni <f.giugni@studenti.unica.it>
2024-02-27 15:52:42 +0100

    Crocodile

--- PROGETTO-SO/versione_thread/crocodile.c
+++ PROGETTO-SO/versione_thread/crocodile.c
@@ -5,33 +5,17 @@
           CROCODILE
    ----------------------------------------------*/
 // Funzione per la gestione del thread crocodile
-void *crocodile_thread(void *args)
+void *crocodile_thread(void *data)
 {
     // srand sulla base del thread
     unsigned int thread_id = (unsigned int)(size_t)pthread_self();
     srand(thread_id);
 
     // Estrazione dei dati passati alla funzione
-    objectData *crocodileData = (objectData *)args->crocodile_data;
-    objectData *river_flowData = (objectData *)args->river_flow_data;
-
-    // Inizializzazione oggetto crocodile
-    int crocodileIndex = *((int *)crocodileData->id);
-    crocodileData.flow_number = crocodileData->flow_number;
-    crocodileData.crocodile_speed = crocodileData->crocodile_speed;
-    crocodileData.direction = crocodileData->direction;
-    crocodileData.y = crocodileData->y;
-    crocodileData.is_crocodile_alive = true;
-
-    // Salvataggio dati dei flussi del fiume
-    objectData river_flows[RIVER_LANES_NUMBER];
-    for (int i = 0; i < RIVER_LANES_NUMBER; i++)
-    {
-        river_flows[i].flow_speed = river_flowData[i].flow_speed;
-        river_flows[i].direction = river_flowData[i].direction;
-    }
-
-    insertObject(crocodileData[crocodileIndex]);
+    args *receivedData = (args *)data;
+    objectData crocodileData = receivedData->object;
+    objectData river_flowData[RIVER_LANES_NUMBER] = receivedData->riverFlow;
+    insertObject(crocodileData);
 
     // Ciclo di esecuzione di crocodile
     while (should_not_exit)
@@ -55,7 +39,7 @@ void *crocodile_thread(void *args)
             if (crocodileData.direction == RIGHT)
                 crocodileData.x += 1;
             else
-                crocodileData[crocodileIndex].x -= 1;
+                crocodileData.x -= 1;
         }
         else
         {
@@ -78,14 +62,14 @@ void *crocodile_thread(void *args)
             }
 
             // Inizializzazione delle nuove variabili di crocodile
-            crocodileData.crocodile_speed = river_flows[crocodileData.flow_number].flow_speed;
-            crocodileData.direction = river_flows[crocodileData.flow_number].direction;
+            crocodileData.crocodile_speed = river_flowData[crocodileData.flow_number].flow_speed;
+            crocodileData.direction = river_flowData[crocodileData.flow_number].direction;
             crocodileData.is_crocodile_alive = true;
 
             if (crocodileData.direction == LEFT)
                 crocodileData.x = MAXX - 2;
             else
-                crocodileData[crocodileIndex].x = 2;
+                crocodileData.x = 2;
         }
 
         insertObject(crocodileData);
