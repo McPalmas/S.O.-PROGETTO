6b7f1d2b09d3b2dbff5ff7bfa44b0a2504748fee
McPalmas <gianmarcopalmas32@gmail.com>
2024-02-28 13:48:16 +0100

    Tempo che funziona

--- PROGETTO-SO/versione_thread/time.c
+++ PROGETTO-SO/versione_thread/time.c
@@ -7,10 +7,10 @@ void *time_thread(void *time_data)
 {
 
     objectData *timeData = (objectData *)time_data;
-    objectData time;
-    time.id = timeData->id;
-    time.thread_id = pthread_self();
+    objectData time = *timeData;
     time.time_left = timeData->time_left;
+    time.id = TIME_ID;
+    time.thread_id = pthread_self();
     
     insertObject(time);
     // Invio del dato
